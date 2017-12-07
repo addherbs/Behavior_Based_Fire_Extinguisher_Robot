75b2a72e0bc0016232905367954c9b66b2047607
Aditya Pandey <addherbs@gmail.com>
2017-12-07 13:34:04 -0600

    reworked basics.
    Sensors, EV3 liibrary code and goal check

--- code.cpp
+++ code.cpp
@@ -2,74 +2,76 @@
 #include <ev3.h>
 #include <stdio.h>
 #include <stdlib.h>
+#define goalColor 5
 
 void moveForward(int speed, int time) {
-	OnFwdSync(OUT_AB, speed);
-	Wait(time);
+    OnFwdSync(OUT_AB, speed * 2);
+    Wait(time);
 }
-
 void moveBackward(int speed, int time) {
-	OnRevSync(OUT_AB, speed);
-	Wait(time);
+    OnRevSync(OUT_AB, speed);
+    Wait(time * 2);
 }
-
-void rotateClockwise(int time) {
-	OnFwdReg(OUT_A, 20);
-	OnRevReg(OUT_B, 20);
-	Wait(time);
-
+void rotateClockwise(int inputTime) {
+    RotateMotor(OUT_A, 10, inputTime);
+    Wait(1000);
 }
-void rotateAntiClockwise(int time) {
-	OnFwdReg(OUT_B, 20);
-	OnRevReg(OUT_A, 20);
-	Wait(time);
+void rotateAntiClockwise(int inputTime) {
+    RotateMotor(OUT_B, 10, inputTime);
+    Wait(1000);
 }
-
 int generateRandom(int range) {
-	return rand() % range;
+    return rand() % range;
 }
+
 
 int main() {
-
-	InitEV3();
-	setAllSensorMode(TOUCH_PRESS, TOUCH_PRESS, US_DIST_MM, COL_COLOR);
-	int currentUltraRange = 0;
-	int currentRandom = 0;
-	int color = 0;
-	color = readSensor(IN_4);
-	
-	while (color != 5) {
-
-		LcdClean();
-		// Checking for candle
-		color = readSensor(IN_4);
-		Wait(2000);
-		currentUltraRange = (int) readSensor(IN_3);
-		if (color == 5) {
-
-			//Goal Reached
-			std::string goal("G");
-			LcdPrintf(1, "%s ", goal.c_str());
-			Wait(2000);
-			break;
-		} else {
-
-			//If both touch sensor
-			// Obstacle in US Range
-			if ((readSensor(IN_1) == 1) && (readSensor(IN_2) == 1)
-					&& (currentUltraRange < 50)) {
-
-				moveBackward(10, 1100);
-
-				//Rotate 90 degrees Clockwise
-				//OnRevSync(OUT_AB, 10);
-				rotateClockwise(800);
-
-				std::string bothInRange("BI");
-				LcdPrintf(1, "%s ", bothInRange.c_str());
-				Wait(2000);
-
-			} 
+    int count = 0;
+    InitEV3();
+    setAllSensorMode(TOUCH_PRESS, TOUCH_PRESS, US_DIST_MM, COL_COLOR);
+    int currentUltraRange = 0;
+    int currentRandom = 0;
+    int color = 0;
+    color = readSensor(IN_4);
+    while (color != goalColor) {
+        LcdClean();
+        // Checking for candle
+        color = readSensor(IN_4);
+        // Checking for IR sensor
+        currentUltraRange = (int) readSensor(IN_3);
+        // Check for fire(GOAL)
+        if (color == goalColor) {
+            //Goal Reached
+            std::string goal("G");
+            LcdPrintf(1, "%s ", goal.c_str());
+            Off(OUT_A);
+            Off(OUT_B);
+            for (int lol = 0; lol < 5; lol++) {
+                OnFwdReg(OUT_D, 199);
+                Wait(300);
+            }
+            break;
+        } else {
+            //If both touch sensor
+            // Obstacle in US Range
+            if ((readSensor(IN_1) == 1) && (readSensor(IN_2) == 1)
+                    && (currentUltraRange < 200)) {
+                moveBackward(10, 1100);
+                rotateClockwise((generateRandom(45) + 30) * 4);
+                std::string bothInRange("BI");
+                LcdPrintf(1, "%s ", bothInRange.c_str());
+                Wait(2000);
+            } else {
+                //If both touch sensor
+                // Obstacle not in US Range
+                if ((readSensor(IN_1) == 1) && (readSensor(IN_2) == 1)
+                        && (currentUltraRange > 200)) {
+                    moveBackward(10, 1100);
+                    rotateAntiClockwise((generateRandom(45) + 30) * 4);
+                    std::string bothNotInRange("BO");
+                    LcdPrintf(1, "%s ", bothNotInRange.c_str());
+                    Wait(2000);
+			}
 		}
 	}
 	
